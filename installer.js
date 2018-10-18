0e71cad66a06691f9d41315cadd19383da1c83d8
Roger Pettett <roger.pettett@nanoporetech.com>
2018-10-18 10:31:43 +0100

    fix app name

--- installer.js
+++ installer.js
@@ -3,11 +3,11 @@ const MSICreator = require('electron-wix-msi').MSICreator;
 async function start () {
     // Step 1: Instantiate the MSICreator
     const msiCreator = new MSICreator({
-	appDirectory: "The Amazing Video Fetcher-win32-x64",
-	description: 'The Amazing Video Fetcher',
-	exe: 'TAVF Installer', // both target installer exe and installed shortcut target. doh.
-	name: 'The Amazing Video Fetcher',
-	manufacturer: 'PsyPhi Network',
+	appDirectory: "Viddle-win32-x64",
+	description: 'Viddle: The Amazing Video Fetcher',
+	exe: 'Viddle', // both target installer exe and installed shortcut target. doh.
+	name: 'Viddle',
+	manufacturer: 'Viddle Central',
 	version: '1.0.0',
 	outputDirectory: '.',
 	upgradeCode: '63D780FC-73B2-4F90-AFD6-5C77895F32CC',
